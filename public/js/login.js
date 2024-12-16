5a664543ad1100824268318f09c6992b649ec75a
HumanEraser <ohdyey10@gmail.com>
2024-12-16 18:57:02 +0800

    asdfasdf

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,6 @@
-function checkLogIn(){
-    
+function checkLogin(){
+    var userN = document.getElementById('username').innerHTML;
+    var passW = document.getElementById('password').innerHTML;
+    console.log(userN);
+    console.log(passW);
 }
