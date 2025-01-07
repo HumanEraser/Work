d41c086f757f9c35fd49a92836b4f36733a4fc3b
milocent <miloceptical@gmail.com>
2025-01-07 13:09:30 +0800

    e

--- inventory.js
+++ inventory.js
@@ -57,6 +57,81 @@ router.get('/transactionList', async function(req,res){
     }
 });
 
+router.post('/transaction', async function(req, res){
+    if(typeof req.session.access == "undefined" && typeof req.body.details != "undefined"){
+        if(req.session.access != "Administrator"){
+            res.status(500).send();
+        }else{
+            var db = await connect("root", "db_aemetal");
+            var details = req.body.details;
+            try{
+                await db.query(
+                    "INSERT INTO tb_transaction (transactionItem, transactionQuantity, transactionDiscount, transactionShippingFee) VALUES(?,?,?,?)"
+                    [details.itemId, details.quantity,details.discount,details.shippingFee]
+                );
+                db.end();
+                res.status(200).send();
+            }catch(err){
+                db.end();
+                console.log(err);
+                res.status(500).send();
+            }
+        }
+    }else{
+        res.status(500).send();
+    }
+});
+
+router.put('/transaction', async function(req, res){
+    if(typeof req.session.access == "undefined" && typeof req.body.details != "undefined"){
+        if(req.session.access != "Administrator"){
+            res.status(500).send();
+        }else{
+            var db = await connect("root", "db_aemetal");
+            var details = req.body.details;
+            try{
+                await db.query(
+                    "UPDATE tb_transaction SET transactionItem = ?, transactionQuantity = ?, transactionDiscount = ?, transactionShippingFee = ? WHERE transactionId = ?"
+                    [details.itemId, details.quantity,details.discount,details.shippingFee, details.id]
+                );
+                db.end();
+                res.status(200).send();
+            }catch(err){
+                db.end();
+                console.log(err);
+                res.status(500).send();
+            }
+        }
+    }else{
+        res.status(500).send();
+    }
+});
+
+router.delete('/transaction', async function(req, res){
+    if(typeof req.session.access == "undefined" && typeof req.body.details != "undefined"){
+        if(req.session.access != "Administrator"){
+            res.status(500).send();
+        }else{
+            var db = await connect("root", "db_aemetal");
+            var details = req.body.details;
+            try{
+                await db.query(
+                    "DELETE FROM tb_transaction WHERE transactionId = ?"
+                    [details.id]
+                );
+                db.end();
+                res.status(200).send();
+            }catch(err){
+                db.end();
+                console.log(err);
+                res.status(500).send();
+            }
+        }
+    }else{
+        res.status(500).send();
+    }
+});
+
 router.get('/inventoryList', async function(req,res){
     if(typeof req.session.access != "undefined" && typeof req.query.page != "undefined"){
         if(req.session.access != "Administrator"){
@@ -82,12 +157,100 @@ router.get('/inventoryList', async function(req,res){
     }
 });
 
+router.post('/inventory', async function(req, res){
+    if(typeof req.session.access == "undefined" && typeof req.body.details != "undefined"){
+        if(req.session.access != "Administrator"){
+            res.status(500).send();
+        }else{
+            var db = await connect("root", "db_aemetal");
+            var details = req.body.details;
+            try{
+                await db.query(
+                    "INSERT INTO tb_inventory (inventoryBrand, inventoryCategory, inventoryPrice, inventoryUnitOfMeasurement) VALUES(?,?,?,?)"
+                    [details.brand, details.category, details.price, details.unit]
+                );
+                db.end();
+                res.status(200).send();
+            }catch(err){
+                db.end();
+                console.log(err);
+                res.status(500).send();
+            }
+        }
+    }else{
+        res.status(500).send();
+    }
+});
+
+router.put('/inventory', async function(req, res){
+    if(typeof req.session.access == "undefined" && typeof req.body.details != "undefined"){
+        if(req.session.access != "Administrator"){
+            res.status(500).send();
+        }else{
+            var db = await connect("root", "db_aemetal");
+            var details = req.body.details;
+            try{
+                await db.query(
+                    "UPDATE tb_inventory SET inventoryBrand = ?, inventoryCategory = ?, inventoryPrice = ?, inventoryUnitOfMeasurement = ? WHERE inventoryId = ?"
+                    [details.brand, details.category, details.price, details.unit, details.id]
+                );
+                db.end();
+                res.status(200).send();
+            }catch(err){
+                db.end();
+                console.log(err);
+                res.status(500).send();
+            }
+        }
+    }else{
+        res.status(500).send();
+    }
+});
+
+router.delete('/inventory', async function(req, res){
+    if(typeof req.session.access == "undefined" && typeof req.body.details != "undefined"){
+        if(req.session.access != "Administrator"){
+            res.status(500).send();
+        }else{
+            var db = await connect("root", "db_aemetal");
+            var details = req.body.details;
+            try{
+                await db.query(
+                    "Delete From tb_inventory WHERE inventoryId = ?"
+                    [details.id]
+                );
+                db.end();
+                res.status(200).send();
+            }catch(err){
+                db.end();
+                console.log(err);
+                res.status(500).send();
+            }
+        }
+    }else{
+        res.status(500).send();
+    }
+});
+
 router.get('/admin', async function(req, res){
-    if(typeof req.session.access == "undefined"){
+    if(typeof req.session.access != "undefined"){
         if(req.session.access != "Administrator"){
             res.redirect("/")
         }else{
             res.render('adminView.ejs');
+        }
+    }else{
+        res.redirect("/");
+    }
+});
+
+
+router.get('/sales', async function(req, res){
+    if(typeof req.session.access != "undefined"){
+        if(req.session.access != "Administrator"){
+            res.redirect("/")
+        }else{
+            res.render('adminViewSales.ejs');
         }
     }else{
         res.redirect("/");
